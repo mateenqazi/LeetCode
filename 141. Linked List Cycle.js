e0fe0d0e730c082b11365bc4d62f649e8101bc9b
Mateen Qazi <57771880+mateenqazi@users.noreply.github.com>
2022-06-12 22:29:58 +0500

    updated

---
+++ 141. Linked List Cycle.js
@@ -0,0 +1,10 @@
+var hasCycle = function(head) {
+    let set = new Set();
+    while (head) {
+        if (set.has(head)) return true;
+        set.add(head);
+        head = head.next;
+    }
+    return false;
+    
+};
