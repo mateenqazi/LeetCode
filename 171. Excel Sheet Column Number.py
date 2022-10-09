dee0867ee5adce27f0414eecbc93af22a0495e03
Mateen Qazi <57771880+mateenqazi@users.noreply.github.com>
2022-10-10 00:31:37 +0500

    171

---
+++ 171. Excel Sheet Column Number.py
@@ -0,0 +1,13 @@
+# Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        alphaToNum = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 
+        'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 
+        'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+        result = 0
+        for index, value in enumerate(columnTitle[::-1]):
+            result += alphaToNum[value] * (26**index)
+        return result
